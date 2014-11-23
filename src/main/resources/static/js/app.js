bcf902065d2172192974f2e39829e31ed4679e46
Mariano Fernández <mariano.fernandez@gmail.com>
2014-11-23 08:32:31 +0100

    Can edit

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -15,6 +15,16 @@
 				$form.submit();
 			});
         });
+
+		var $toggleItems = qsa('#todo-list li label');
+		$toggleItems.forEach(function($item) {
+			$on($item, 'dblclick', function () {
+				var $form = this.form;
+				var id = this.getAttribute('data-id');
+				var $li = qs('li[data-id="' + id + '"]');
+				$li.className = $li.className + ' editing';
+			});
+        });
 	});
 
 })();
